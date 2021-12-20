dce5eebdef050e5c3d3e3b2178d8ddfb59d79904
윤수현 <yoonsuhyeon@yunsuhyeon-ui-MacBookPro.local>
2021-12-20 22:52:02 +0900

    기본 회원 클래스 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,34 @@
 package hellojpa;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //하나만 생성되고 애플리케이션 전체에서 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        //쓰레드간에 공유 X
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            //insert
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("TEST");
+            Member member = em.find(Member.class, 1L);
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            member.setName("Change");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
