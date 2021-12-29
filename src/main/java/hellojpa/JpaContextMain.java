4f11c4a0cc75abdb1aab20d27fb114e8fa78b515
YoonSuHyeon <96-05-03@naver.com>
2021-12-29 21:02:28 +0900

    영속성 컨텍스트

---
+++ src/main/java/hellojpa/JpaContextMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaContextMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+            //영속
+            em.persist(member);
+            //1차  캐시 사용
+            Member findMember = em.find(Member.class, 100L);
+//            //준영속
+//            em.detach(member);
+//            //삭제
+//            em.remove(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+    }
+}
